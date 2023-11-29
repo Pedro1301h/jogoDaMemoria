b288673233118ecbc40981852fe289109f45ca68
Pedro <pedro1301@gmail.com>
2023-11-29 17:16:49 -0300

    Mais alguns ajustes básicos

--- script.js
+++ script.js
@@ -4,6 +4,5 @@ let cartas = document.querySelectorAll('.cartas')
 
 //  Embaralhando as cartas
 for(let i=0;i < cartas.length;i++){
-    cartas[i].style.order = Math.floor(Math.random() * 11)
+    cartas[i].style.order = Math.floor(Math.random() * cartas.length-1)
 }
-
