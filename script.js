18128efb31c919cdf80d9055ba812b0ba835dbe9
Pedro <pedro1301@gmail.com>
2023-11-30 08:41:25 -0300

    Adicionando getAttibute no script

--- script.js
+++ script.js
@@ -7,40 +7,60 @@ for(let i=0;i < cartas.length;i++){
     cartas[i].style.order = Math.floor(Math.random() * cartas.length)
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //Girando as cartas
 
 for(let i = 0; i < cartas.length; i++){
     cartas[i].addEventListener('click',clicar)
 
     function clicar(){
-        if(i == 0 || i == 1){
-            cartas[i].style.background='beige'
+
+        console.log(cartas[i].getAttribute('class'))
+
+        if(cartas[i].getAttribute('class') == 'cartas comb1'){
+            cartas[i].style.background='white'
             cartas[i].style.border='none'
             cartas[i].style.backgroundImage='url("imagens/css_scale.png")'
         }
-        if(i == 2 || i == 3){
+        if(cartas[i].getAttribute('class') == 'cartas comb2'){
+            cartas[i].style.background='white'
             cartas[i].style.border='none'
-            cartas[i].style.background='beige'
             cartas[i].style.backgroundImage='url("imagens/html_scale.png")'
         }
-        if(i == 4 || i == 5){
-            cartas[i].style.border='none'
+        if(cartas[i].getAttribute('class') == 'cartas comb3'){
             cartas[i].style.background='beige'
+            cartas[i].style.border='none'
             cartas[i].style.backgroundImage='url("imagens/js_scale.png")'
         }
-        if(i == 6 || i == 7){
-            cartas[i].style.border='none'
+        if(cartas[i].getAttribute('class') == 'cartas comb4'){
             cartas[i].style.background='beige'
+            cartas[i].style.border='none'
             cartas[i].style.backgroundImage='url("imagens/php_scale.png")'
         }
-        if(i == 8 || i == 9){
-            cartas[i].style.border='none'
+        if(cartas[i].getAttribute('class') == 'cartas comb5'){
             cartas[i].style.background='beige'
+            cartas[i].style.border='none'
             cartas[i].style.backgroundImage='url("imagens/java_scale.png")'
         }
-        if(i == 10 || i == 11){
-            cartas[i].style.border='none'
+        if(cartas[i].getAttribute('class') == 'cartas comb6'){
             cartas[i].style.background='beige'
+            cartas[i].style.border='none'
             cartas[i].style.backgroundImage='url("imagens/react_scale.png")'
         }
     }
